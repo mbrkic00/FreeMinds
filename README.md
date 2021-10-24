72c7596d16946082ca077c9d6a3ee4baf5d4b3ac
KarloMijaljevic <karlo.mijaljevic@gmail.com>
2021-10-24 15:12:57 +0200

    Express and React initialised with proxy in place

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FreeMinds
+
+A free online learning platform. This app is a project by Karlo Mijaljević
+and Marta Brkić for their University class of Program Engineering In Telecommunication's.
---
+++ server.js
@@ -0,0 +1,7 @@
+// ===== Express app =====
+const express = require("express");
+const app = express();
+
+// ===== PORT listen =====
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
