8cb4689b4a1722aa40c670204380ae258cb33e0b
KarloMijaljevic <karlo.mijaljevic@gmail.com>
2021-10-26 16:52:28 +0200

    Added mongoose as a DB and created a User model

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+// ===== Environment variables =====
+const dotenv = require('dotenv');
+dotenv.config();
+const db = process.env.MONGO_URI;
+
+// ===== MongoDB setup =====
+const mongoose = require('mongoose');
+mongoose
+   .connect(db)
+   .then(() => console.log('MongoDb Connected...'))
+   .catch(err => console.log(err))
+
 // ===== Express app =====
 const express = require("express");
 const app = express();
+
+// ===== Routes =====
+app.use('/users', require('./routes/users'))
 
 // ===== PORT listen =====
 const PORT = process.env.PORT || 4000
