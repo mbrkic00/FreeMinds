1cba96572d895403db055280ac35843b608b8ab2
KarloMijaljevic <karlo.mijaljevic@gmail.com>
2021-10-27 18:02:57 +0200

    Starded connection to Heroku for app production deployment.

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ const app = express();
 // ===== Routes =====
 app.use('/users', require('./routes/users'))
 
+// ===== Production =====
+const path = require('path')
+if (process.env.NODE_ENV === 'production') {
+   // Set static folder
+   app.use(express.static('client/build'))
+
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+   })
+}
+
 // ===== PORT listen =====
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
